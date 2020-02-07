e219eea3d00440369f62d416ab51e75436f481db
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-02-07 19:03:24 +0900

    refactor macro tests

--- macros/src/imp.rs
+++ macros/src/imp.rs
@@ -27,276 +27,34 @@ pub(crate) fn test_case(args: TokenStream, item: TokenStream) -> Result<TokenStr
 mod tests {
     use super::*;
     use quote::quote;
+    use std::path::Path;
 
-    #[test]
-    fn test_sync() {
-        let args = TokenStream::new();
-        let item = quote! {
-            fn case_sync() {
-                let mut vec = vec![0usize; 5];
-                assert_eq!(vec.len(), 5);
-                assert!(vec.capacity() >= 5);
+    fn read_file<P: AsRef<Path>>(path: P, expand_braces: bool) -> TokenStream {
+        let mut content = std::fs::read_to_string(path).unwrap();
+        if expand_braces {
+            content = content
+                .replace("{{section}}", CURRENT_SECTION_IDENT_NAME)
+                .replace("{{inner}}", INNER_FN_IDENT_NAME);
+        }
+        content.parse().unwrap()
+    }
 
-                section!("resizing bigger changes size and capacity", {
-                    vec.resize(10, 0);
-                    assert_eq!(vec.len(), 10);
-                    assert!(vec.capacity() >= 5);
-                });
-            }
-        };
-
-        let section = Ident::new(CURRENT_SECTION_IDENT_NAME, Span::call_site());
-        let inner = Ident::new(INNER_FN_IDENT_NAME, Span::call_site());
-        let expected = quote! {
-            #[test]
-            fn case_sync() {
-                fn #inner(__section: rye::_internal::Section) {
-                    #[allow(unused_mut, unused_variables)]
-                    let mut #section = __section;
-                    {
-                        let mut vec = vec![0usize; 5];
-                        assert_eq!(vec.len(), 5);
-                        assert!(vec.capacity() >= 5);
-
-                        {
-                            static SECTION: rye::_internal::SectionId = rye::_internal::SectionId::SubSection {
-                                name: "resizing bigger changes size and capacity",
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            };
-                            if let Some(__section) = #section.new_section(&SECTION) {
-                                let mut #section = __section;
-                                {
-                                    vec.resize(10, 0);
-                                    assert_eq!(vec.len(), 10);
-                                    assert!(vec.capacity() >= 5);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                #[allow(unused_mut)]
-                let mut test_case = rye::_internal::TestCase::new();
-                while !test_case.completed() {
-                    #inner(test_case.root_section());
-                }
-            }
-        };
-
+    fn test_expanded(name: &str, args: impl Into<Option<TokenStream>>) {
+        let args = args.into().unwrap_or_else(TokenStream::new);
+        let item = read_file(format!("test/{}.in", name), false);
+        let expected = read_file(format!("test/{}.out", name), true);
         let output = test_case(args, item).unwrap();
         assert_eq!(expected.to_string(), output.to_string());
     }
 
     #[test]
-    fn test_nested() {
-        let args = TokenStream::new();
-        let item = quote! {
-            fn case_sync() {
-                let mut vec = vec![0usize; 5];
-                assert_eq!(vec.len(), 5);
-                assert!(vec.capacity() >= 5);
-
-                section!("resizing bigger changes size and capacity", {
-                    vec.resize(10, 0);
-                    assert_eq!(vec.len(), 10);
-                    assert!(vec.capacity() >= 10);
-
-                    section!("shrinking smaller does not changes capacity", {
-                        vec.resize(0, 0);
-                        assert_eq!(vec.len(), 0);
-                        assert!(vec.capacity() >= 10);
-                    });
-                });
-            }
-        };
-
-        let section = Ident::new(CURRENT_SECTION_IDENT_NAME, Span::call_site());
-        let inner = Ident::new(INNER_FN_IDENT_NAME, Span::call_site());
-        let expected = quote! {
-            #[test]
-            fn case_sync() {
-                fn #inner(__section: rye::_internal::Section) {
-                    #[allow(unused_mut, unused_variables)]
-                    let mut #section = __section;
-                    {
-                        let mut vec = vec![0usize; 5];
-                        assert_eq!(vec.len(), 5);
-                        assert!(vec.capacity() >= 5);
-
-                        {
-                            static SECTION: rye::_internal::SectionId = rye::_internal::SectionId::SubSection {
-                                name: "resizing bigger changes size and capacity" ,
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            };
-                            if let Some(__section) = #section.new_section(&SECTION) {
-                                let mut #section = __section;
-                                {
-                                    vec.resize(10, 0);
-                                    assert_eq!(vec.len(), 10);
-                                    assert!(vec.capacity() >= 10);
-
-                                    {
-                                        static SECTION: rye::_internal::SectionId = rye::_internal::SectionId::SubSection {
-                                            name: "shrinking smaller does not changes capacity" ,
-                                            file: file!(),
-                                            line: line!(),
-                                            column: column!(),
-                                        };
-                                        if let Some(__section) = #section.new_section(&SECTION) {
-                                            let mut #section = __section;
-                                            {
-                                                vec.resize(0, 0);
-                                                assert_eq!(vec.len(), 0);
-                                                assert!(vec.capacity() >= 10);
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                #[allow(unused_mut)]
-                let mut test_case = rye::_internal::TestCase::new();
-                while !test_case.completed() {
-                    #inner(test_case.root_section());
-                }
-            }
-        };
-
-        let output = test_case(args, item).unwrap();
-        assert_eq!(expected.to_string(), output.to_string());
-    }
-
-    #[test]
-    fn test_async() {
-        let args = TokenStream::new();
-        let item = quote! {
-            async fn case_async() {
-                let mut vec = vec![0usize; 5];
-                assert_eq!(vec.len(), 5);
-                assert!(vec.capacity() >= 5);
-
-                section!("resizing bigger changes size and capacity", {
-                    vec.resize(10, 0);
-                    assert_eq!(vec.len(), 10);
-                    assert!(vec.capacity() >= 5);
-                });
-            }
-        };
-
-        let section = Ident::new(CURRENT_SECTION_IDENT_NAME, Span::call_site());
-        let inner = Ident::new(INNER_FN_IDENT_NAME, Span::call_site());
-        let expected = quote! {
-            #[test]
-            fn case_async() {
-                async fn #inner(__section: rye::_internal::Section) {
-                    #[allow(unused_mut, unused_variables)]
-                    let mut #section = __section;
-                    {
-                        let mut vec = vec![0usize; 5];
-                        assert_eq!(vec.len(), 5);
-                        assert!(vec.capacity() >= 5);
-
-                        {
-                            static SECTION: rye::_internal::SectionId = rye::_internal::SectionId::SubSection {
-                                name: "resizing bigger changes size and capacity",
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            };
-                            if let Some(__section) = #section.new_section(&SECTION) {
-                                let mut #section = __section;
-                                {
-                                    vec.resize(10, 0);
-                                    assert_eq!(vec.len(), 10);
-                                    assert!(vec.capacity() >= 5);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                block_on(async {
-                    #[allow(unused_mut)]
-                    let mut test_case = rye::_internal::TestCase::new();
-                    while !test_case.completed() {
-                        #inner(test_case.root_section()).await;
-                    }
-                });
-            }
-        };
-
-        let output = test_case(args, item).unwrap();
-        assert_eq!(expected.to_string(), output.to_string());
-    }
-
-    #[test]
-    fn test_async_with_args() {
-        let args = quote!(block_on = "path::to::custom_block_on");
-        let item = quote! {
-            async fn case_async() {
-                let mut vec = vec![0usize; 5];
-                assert_eq!(vec.len(), 5);
-                assert!(vec.capacity() >= 5);
-
-                section!("resizing bigger changes size and capacity", {
-                    vec.resize(10, 0);
-                    assert_eq!(vec.len(), 10);
-                    assert!(vec.capacity() >= 5);
-                });
-            }
-        };
-
-        let section = Ident::new(CURRENT_SECTION_IDENT_NAME, Span::call_site());
-        let inner = Ident::new(INNER_FN_IDENT_NAME, Span::call_site());
-        let expected = quote! {
-            #[test]
-            fn case_async() {
-                async fn #inner(__section: rye::_internal::Section) {
-                    #[allow(unused_mut, unused_variables)]
-                    let mut #section = __section;
-                    {
-                        let mut vec = vec![0usize; 5];
-                        assert_eq!(vec.len(), 5);
-                        assert!(vec.capacity() >= 5);
-
-                        {
-                            static SECTION: rye::_internal::SectionId = rye::_internal::SectionId::SubSection {
-                                name: "resizing bigger changes size and capacity",
-                                file: file!(),
-                                line: line!(),
-                                column: column!(),
-                            };
-                            if let Some(__section) = #section.new_section(&SECTION) {
-                                let mut #section = __section;
-                                {
-                                    vec.resize(10, 0);
-                                    assert_eq!(vec.len(), 10);
-                                    assert!(vec.capacity() >= 5);
-                                }
-                            }
-                        }
-                    }
-                }
-
-                path::to::custom_block_on(async {
-                    #[allow(unused_mut)]
-                    let mut test_case = rye::_internal::TestCase::new();
-
-                    while !test_case.completed() {
-                        #inner(test_case.root_section()).await;
-                    }
-                });
-            }
-        };
-
-        let output = test_case(args, item).unwrap();
-        assert_eq!(expected.to_string(), output.to_string());
+    fn test_suite() {
+        test_expanded("01-sync", None);
+        test_expanded("02-nested", None);
+        test_expanded("03-async", None);
+        test_expanded(
+            "04-async-with-args",
+            quote!(block_on = "path::to::custom_block_on"),
+        );
     }
 }
