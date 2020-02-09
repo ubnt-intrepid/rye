ba1351670157882d33c66ab8c18a61ac612d50c7
Yusuke Sasaki <yusuke.sasaki.nuem@gmail.com>
2020-02-09 21:31:12 +0900

    forbid sections in loop

--- macros/src/expand.rs
+++ macros/src/expand.rs
@@ -4,7 +4,7 @@ use std::mem;
 use syn::{
     parse::{Parse, ParseStream, Result},
     visit_mut::{self, VisitMut},
-    Block, Expr, ItemFn, Macro, Stmt, Token,
+    Block, Expr, ExprForLoop, ExprLoop, ExprWhile, ItemFn, Macro, Stmt, Token,
 };
 
 struct SectionBody {
@@ -27,17 +27,18 @@ struct ExpandBlock {
     sections: IndexMap<SectionId, Section>,
     next_section_id: SectionId,
     current_section_id: Option<SectionId>,
+    in_loop: bool,
 }
 
 impl ExpandBlock {
-    fn expand_section_macro(&mut self, mac: &Macro) -> (Stmt, Option<SectionId>) {
-        let body: SectionBody = match mac.parse_body() {
-            Ok(body) => body,
-            Err(err) => {
-                let err = err.to_compile_error();
-                return (syn::parse_quote!(#err), None);
-            }
-        };
+    fn expand_section_macro(&mut self, mac: &Macro) -> Result<(Stmt, SectionId)> {
+        if self.in_loop {
+            return Err(syn::parse::Error::new_spanned(
+                mac,
+                "section cannot be described in a loop",
+            ));
+        }
+        let body: SectionBody = mac.parse_body()?;
 
         let name = &body.name;
         let block = &body.block;
@@ -63,12 +64,12 @@ impl ExpandBlock {
         );
         self.next_section_id += 1;
 
-        (
+        Ok((
             syn::parse_quote! {
                 if rye::_internal::is_target(#section_id) #block
             },
-            Some(section_id),
-        )
+            section_id,
+        ))
     }
 }
 
@@ -77,8 +78,14 @@ impl VisitMut for ExpandBlock {
         let section_id = match item {
             Stmt::Expr(Expr::Macro(expr_macro)) | Stmt::Semi(Expr::Macro(expr_macro), _) => {
                 if expr_macro.mac.path.is_ident("section") {
-                    let (expanded, section_id) = self.expand_section_macro(&expr_macro.mac);
-                    mem::replace(item, expanded);
+                    let (stmt, section_id) = match self.expand_section_macro(&expr_macro.mac) {
+                        Ok((expanded, section_id)) => (expanded, Some(section_id)),
+                        Err(err) => {
+                            let err = err.to_compile_error();
+                            (syn::parse_quote!(#err), None)
+                        }
+                    };
+                    mem::replace(item, stmt);
                     section_id
                 } else {
                     None
@@ -94,6 +101,24 @@ impl VisitMut for ExpandBlock {
             visit_mut::visit_stmt_mut(self, item);
         }
     }
+
+    fn visit_expr_for_loop_mut(&mut self, node: &mut ExprForLoop) {
+        let prev = std::mem::replace(&mut self.in_loop, true);
+        visit_mut::visit_expr_for_loop_mut(self, node);
+        self.in_loop = prev;
+    }
+
+    fn visit_expr_loop_mut(&mut self, node: &mut ExprLoop) {
+        let prev = std::mem::replace(&mut self.in_loop, true);
+        visit_mut::visit_expr_loop_mut(self, node);
+        self.in_loop = prev;
+    }
+
+    fn visit_expr_while_mut(&mut self, node: &mut ExprWhile) {
+        let prev = std::mem::replace(&mut self.in_loop, true);
+        visit_mut::visit_expr_while_mut(self, node);
+        self.in_loop = prev;
+    }
 }
 
 #[inline]
@@ -102,6 +127,7 @@ pub(crate) fn expand(item: &mut ItemFn) -> Vec<Section> {
         sections: IndexMap::new(),
         next_section_id: 0,
         current_section_id: None,
+        in_loop: false,
     };
     expand.visit_block_mut(&mut *item.block);
     expand.sections.into_iter().map(|(_k, v)| v).collect()
---
+++ tests/ui/03-section-in-loop.rs
@@ -0,0 +1,28 @@
+fn main() {}
+
+#[rye::test_case]
+fn section_in_loop() {
+    loop {
+        section!("section", {
+            assert!(true);
+        });
+    }
+}
+
+#[rye::test_case]
+fn section_in_for_loop() {
+    for _ in 0..10 {
+        section!("section", {
+            assert!(true);
+        });
+    }
+}
+
+#[rye::test_case]
+fn section_in_while_loop() {
+    while false {
+        section!("section", {
+            assert!(true);
+        });
+    }
+}
